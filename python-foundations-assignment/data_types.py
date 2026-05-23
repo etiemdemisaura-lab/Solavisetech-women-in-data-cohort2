8505779cc3fead8853eaf95a9c2ccde4689a118e
Isaura Etiendem Tanyika <etiemdemisaura@gmail.com>
2026-05-23 21:30:06 +0200

    Implement personal bio and utility functions
    Added a personal bio generator, type checker, user information prompts, and a temperature converter.

--- python-foundations-assignment/data_types.py
+++ python-foundations-assignment/data_types.py
@@ -1,1 +1,46 @@
+# Personal bio generator
 
+Name = 'My name is Isaura, I am'
+Age = 31
+Introduction = 'years old. I am'
+height = 1.72
+measurement = 'meters tall,'
+Interest = 'my favorite tech field is data analysis. It is'
+it_is = 'true that'
+student_status = 'I am a student.'
+print(str(Name) + " " + str(Age) + " " + str(Introduction) + " " + str(height) + " " + str(measurement) + " " + str(Interest) + " " + str(it_is) + " " + str(student_status))
+
+# Type checker
+
+Name = 'Isaura'
+print(type(Name))
+Age = 31
+print(type(Age))
+height = 1.72
+print(type(height))
+is_student = 'true'
+print(type(is_student))
+
+# Data conversion
+
+print str(age))
+height = 1.72
+print str(height))
+is_student = 'true'
+print str(is_student))
+
+# User information
+
+Email = input("Please enter your email address: ")
+print("Your email address is: " + Email)
+phone_number = input("Please enter your phone number: ")
+print("Your phone number is: " + phone_number)
+address = input("Please enter your address: ")
+print("Your address is: " + address)
+print( "Your email address is: " + Email + "\nYour phone number is: " + phone_number + "\nYour address is: " + address)
+
+# Temperature converter
+
+celcius = float(input("Enter the temperature in Celcius: "))
+fahrenheit = (celcius * 9/5) + 32
+print(f"{celcius} degrees Celcius is equal to {fahrenheit} degrees Fahrenheit.")
