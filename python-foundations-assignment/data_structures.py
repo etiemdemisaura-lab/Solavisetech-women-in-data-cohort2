5f1a1a45bc260371f082801caee8fe12e3f986d1
Isaura Etiendem Tanyika <etiemdemisaura@gmail.com>
2026-05-24 00:16:56 +0200

    Implement data structures for various examples
    Added data structures for favorite tools, student scores, shopping list, and country capitals.

---
+++ python-foundations-assignment/data_structures.py
@@ -0,0 +1,36 @@
+# Favourite tools
+
+Tools = {'anaconda', 'Github', 'git'}
+tools.append('git')
+tools.remove('anaconda')
+print("Favorite Tools:", tools)
+
+# Student scores
+
+Scores = [25, 30, 28, 35, 40]
+print(Scores)
+print(highest_score)
+print(lowest_score)
+print(average_score)
+
+Shopping list manager
+
+shopping_list = []
+
+shopping_list.append('sugar')
+shopping_list.append('onions')
+shopping_list.append('salt')
+print(shopping_list)
+
+# Country Capitals
+
+countries = (('Cameroon', 'Yaounde')),
+    ('Ghana', 'Accra')),
+    ('Nigeria', 'Abuja'))
+print(("Country Capitals:", countries))
+
+
+
+
+
+
