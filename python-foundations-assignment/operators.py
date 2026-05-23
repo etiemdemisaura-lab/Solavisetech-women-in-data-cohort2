17bf4d2786a660471f3c2668332b5e94ef1b1694
Isaura Etiendem Tanyika <etiemdemisaura@gmail.com>
2026-05-23 22:02:32 +0200

    Add basic calculator and area calculations

---
+++ python-foundations-assignment/operators.py
@@ -0,0 +1,61 @@
+## Simple Calculator
+
+x = 3
+y = 4.5
+print(x + y)  # Output: 7.5
+print(y - x)  # Output: 1.5
+print(x * y)  # Output: 13.5
+print(y / x)  # Output: 1.5
+print(x ** 2)  # Output: 9
+
+
+## Area of Shapes
+
+
+# Area of rectangle
+
+Length = 12
+Width = 8
+Area = Length * Width
+print("The area of the rectangle is:", Area)
+
+# Area of a circle
+
+radius = 35
+Area_Circle = 3.14159 * radius ** 2
+print("The area of the circle is:", Area_Circle)
+
+# Area of a triangle
+base = 7
+height = 10
+Area_Triangle = 0.5 * base * height
+print("The area of the triangle is:", Area_Triangle)
+
+## Even or Odd
+
+number = int(input("Enter a number:"))
+if number % 2 == 0:
+    print("The number is even.")
+else:    print("The number is odd.")
+
+## Student grade percentage
+
+student_score = 150
+total_score = 200
+percentage = (student_score / total_score) * 100
+print("Student's percentage:", percentage)
+
+## BMI calculator 
+
+weight = 98 
+height = 1.82
+bmi = weight / (height ** 2)
+print("BMI:", bmi)
+
+## Power and Modulus
+
+a = 58
+b = 3.14
+print(a**2)
+print(b**2)
+print(15 % a)
