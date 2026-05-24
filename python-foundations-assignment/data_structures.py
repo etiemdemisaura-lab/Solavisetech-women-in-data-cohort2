e4f8b38eece1c7c8321c50dacddfc3ac7cd50ec4
Isaura Etiendem Tanyika <etiemdemisaura@gmail.com>
2026-05-24 20:57:24 +0200

    Add unique visitors and common skills functionality

--- python-foundations-assignment/data_structures.py
+++ python-foundations-assignment/data_structures.py
@@ -15,7 +15,7 @@ print(average_score)
 
 Shopping list manager
 
-shopping_list = []
+ Unique
 
 shopping_list.append('sugar')
 shopping_list.append('onions')
@@ -24,10 +24,43 @@ print(shopping_list)
 
 # Country Capitals
 
-countries = (('Cameroon', 'Yaounde')),
-    ('Ghana', 'Accra')),
-    ('Nigeria', 'Abuja'))
-print(("Country Capitals:", countries))
+countries = (('Cameroon', 'Yaounde'),
+              ('Ghana', 'Accra'),
+              ('Nigeria', 'Abuja'))
+print("Country Capitals:", countries)
+
+# Unique Visitors
+
+visitors = ['tina', 'alex', 'meindl', 'meindl']
+unique_visitors = set(visitors)
+print(unique_visitors)
+
+# Common skills
+
+skills1 = ['biology', 'chemistry', 'physics']
+skills2 = ['math', 'computer science', 'biology']
+
+common_skills = set(skills1) .intersection(set(skills2))
+print(common_skills)
+
+# Student record
+
+name = 'Sandra'
+height = 1.65
+age = 30
+student_record = (name, height, age)
+print(student_record)
+
+# Mini contact book
+
+contact = {'tina': '123-456-7890', 'bob': '987-654-3210'}
+print(contact['tina'])
+search = input('Enter a name to search for: ')
+print(contact[search])
+if search in contact:
+    print(contact[search])
+else:
+    print('Name not found in contact list.')
 
 
 
